691c89ef682a141500bbd5ac5ae169f3ddda0797
«strukovsa» <serrstr07@gmail.com>
2024-03-20 15:20:59 +0300

    users v4

--- dags/examples/stg/bonus_system_users_dag/__init__.py
+++ dags/examples/stg/bonus_system_users_dag/__init__.py
@@ -3,3 +3,4 @@ from __future__ import annotations
 from airflow.decorators import dag, task
 
 from examples.stg.bonus_system_users_dag.bonus_system_users_dag import bonus_system_users_dag
+from stg import EtlSetting, StgEtlSettingsRepository
