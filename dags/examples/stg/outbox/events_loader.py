139543d8622c9c64eb48def4f352395b4c8f0fa3
«strukovsa» <serrstr07@gmail.com>
2024-04-12 16:58:28 +0300

    events v1

---
+++ dags/examples/stg/outbox/events_loader.py
@@ -0,0 +1,105 @@
+from logging import Logger
+from typing import List
+
+import sys
+sys.path.insert(0,'/lessons/dags/examples/')
+from stg import EtlSetting, StgEtlSettingsRepository
+from lib import PgConnect
+from lib.dict_util import json2str
+from psycopg import Connection
+from psycopg.rows import class_row
+from pydantic import BaseModel
+import datetime
+
+
+class EventsObj(BaseModel):
+    id: int
+    event_ts: datetime
+    event_type: str
+    event_value: str
+
+class EventsOriginRepository:
+    def __init__(self, pg: PgConnect) -> None:
+        self._db = pg
+
+    def list_events(self, user_threshold: int, limit: int) -> List[EventsObj]:
+        with self._db.client().cursor(row_factory=class_row(EventsObj)) as cur:
+            cur.execute(
+                """
+                    SELECT id, order_user_id
+                    FROM users
+                    WHERE id > %(threshold)s --Пропускаем те объекты, которые уже загрузили.
+                    ORDER BY id ASC --Обязательна сортировка по id, т.к. id используем в качестве курсора.
+                    LIMIT %(limit)s; --Обрабатываем только одну пачку объектов.
+                """, {
+                    "threshold": user_threshold,
+                    "limit": limit
+                }
+            )
+            objs = cur.fetchall()
+        return objs
+
+
+class UsersDestRepository:
+
+    def insert_users(self, conn: Connection, users: UsersObj) -> None:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                    INSERT INTO stg.bonussystem_users(id, order_user_id)
+                    VALUES (%(id)s, %(order_user_id)s)
+                    ON CONFLICT (id) DO UPDATE
+                    SET
+                        order_user_id = EXCLUDED.order_user_id;
+                """,
+                {
+                    "id": users.id,
+                    "order_user_id": users.order_user_id
+                },
+            )
+
+
+class UsersLoader:
+    WF_KEY = "example_users_origin_to_stg_workflow"
+    LAST_LOADED_ID_KEY = "last_loaded_id"
+    BATCH_LIMIT = 100  
+
+    def __init__(self, pg_origin: PgConnect, pg_dest: PgConnect, log: Logger) -> None:
+        self.pg_dest = pg_dest
+        self.origin = UsersOriginRepository(pg_origin)
+        self.stg = UsersDestRepository()
+        self.settings_repository = StgEtlSettingsRepository()
+        self.log = log
+
+    def load_users(self):
+        # открываем транзакцию.
+        # Транзакция будет закоммичена, если код в блоке with пройдет успешно (т.е. без ошибок).
+        # Если возникнет ошибка, произойдет откат изменений (rollback транзакции).
+        with self.pg_dest.connection() as conn:
+
+            # Прочитываем состояние загрузки
+            # Если настройки еще нет, заводим ее.
+            wf_setting = self.settings_repository.get_setting(conn, self.WF_KEY)
+            if not wf_setting:
+                wf_setting = EtlSetting(id=0, workflow_key=self.WF_KEY, workflow_settings={self.LAST_LOADED_ID_KEY: -1})
+
+            # Вычитываем очередную пачку объектов.
+            last_loaded = wf_setting.workflow_settings[self.LAST_LOADED_ID_KEY]
+            load_queue = self.origin.list_users(last_loaded, self.BATCH_LIMIT)
+            self.log.info(f"Found {len(load_queue)} users to load.")
+            if not load_queue:
+                self.log.info("Quitting.")
+                return
+
+            # Сохраняем объекты в базу dwh.
+            for users in load_queue:
+                self.stg.insert_users(conn, users)
+
+            # Сохраняем прогресс.
+            # Мы пользуемся тем же connection, поэтому настройка сохранится вместе с объектами,
+            # либо откатятся все изменения целиком.
+            wf_setting.workflow_settings[self.LAST_LOADED_ID_KEY] = max([t.id for t in load_queue])
+            wf_setting_json = json2str(wf_setting.workflow_settings)  # Преобразуем к строке, чтобы положить в БД.
+            self.settings_repository.save_setting(conn, wf_setting.workflow_key, wf_setting_json)
+
+            self.log.info(f"Load finished on {wf_setting.workflow_settings[self.LAST_LOADED_ID_KEY]}")
---
+++ dags/examples/stg/outbox/outbox_dag.py
@@ -0,0 +1,58 @@
+import logging
+import sys
+sys.path.insert(0,'/lessons/dags/examples/')
+
+import pendulum
+from airflow.decorators import dag, task
+from examples.stg.bonus_system_ranks_dag.ranks_loader import RankLoader
+from examples.stg.bonus_system_users_dag.users_loader import UsersLoader
+from examples.stg.bonus_system_users_dag.outbox import EventsLoader
+from lib import ConnectionBuilder
+
+log = logging.getLogger(__name__)
+
+
+@dag(
+    schedule_interval='0/15 * * * *',  # Задаем расписание выполнения дага - каждый 15 минут.
+    start_date=pendulum.datetime(2022, 5, 5, tz="UTC"),  # Дата начала выполнения дага. Можно поставить сегодня.
+    catchup=False,  # Нужно ли запускать даг за предыдущие периоды (с start_date до сегодня) - False (не нужно).
+    tags=['sprint5', 'stg', 'origin', 'example'],  # Теги, используются для фильтрации в интерфейсе Airflow.
+    is_paused_upon_creation=True  # Остановлен/запущен при появлении. Сразу запущен.
+)
+def sprint5_example_stg_bonus_system_users_ranks_dag():
+    # Создаем подключение к базе dwh.
+    dwh_pg_connect = ConnectionBuilder.pg_conn("PG_WAREHOUSE_CONNECTION")
+
+    # Создаем подключение к базе подсистемы бонусов.
+    origin_pg_connect = ConnectionBuilder.pg_conn("PG_ORIGIN_BONUS_SYSTEM_CONNECTION")
+
+    # Объявляем таск, который загружает данные.
+    @task(task_id="ranks_load")
+    def load_ranks():
+        # создаем экземпляр класса, в котором реализована логика.
+        rest_loader = RankLoader(origin_pg_connect, dwh_pg_connect, log)
+        rest_loader.load_ranks()  # Вызываем функцию, которая перельет данные.
+
+    @task(task_id="users_load")
+    def load_users():
+        # создаем экземпляр класса, в котором реализована логика.
+        rest_loader = UsersLoader(origin_pg_connect, dwh_pg_connect, log)
+        rest_loader.load_users()  # Вызываем функцию, которая перельет данные.
+
+     @task(task_id="events_load")
+    def load_events():
+        # создаем экземпляр класса, в котором реализована логика.
+        rest_loader = EventsLoader(origin_pg_connect, dwh_pg_connect, log)
+        rest_loader.load_events()  # Вызываем функцию, которая перельет данные.
+
+    # Инициализируем объявленные таски.
+    users_dict = load_users()
+    ranks_dict = load_ranks()
+    events_load = load_events()
+
+    # Далее задаем последовательность выполнения тасков.
+    
+    ranks_dict >> users_dict >> events_load # type: ignore
+
+
+stg_bonus_system_users_ranks_dag = sprint5_example_stg_bonus_system_users_ranks_dag()
