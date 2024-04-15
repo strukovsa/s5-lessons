df2a115254f5b6f80d3fe1be80ca36b4827fd833
«strukovsa» <serrstr07@gmail.com>
2024-04-15 15:14:33 +0300

    events v5

--- dags/examples/stg/outbox/events_loader.py
+++ dags/examples/stg/outbox/events_loader.py
@@ -47,12 +47,7 @@ class EventsDestRepository:
             cur.execute(
                 """
                     INSERT INTO stg.bonussystem_events(id, event_ts, event_type, event_value)
-                    VALUES (%(id)s, %(event_ts)s, %(event_type)s, %(event_value)s)
-                    ON CONFLICT (id) DO UPDATE
-                    SET
-                        event_ts = EXCLUDED.event_ts,
-                        event_type = EXCLUDED.event_type,
-                        event_value = EXCLUDED.event_value;
+                    VALUES (%(id)s, %(event_ts)s, %(event_type)s, %(event_value)s);
                 """,
                 {
                     "id": events.id,
