15c34291c95aaeb8729d0aab353de3724b5698de
«strukovsa» <serrstr07@gmail.com>
2024-04-15 12:35:19 +0300

    events v4

--- dags/examples/stg/outbox/events_loader.py
+++ dags/examples/stg/outbox/events_loader.py
@@ -50,8 +50,8 @@ class EventsDestRepository:
                     VALUES (%(id)s, %(event_ts)s, %(event_type)s, %(event_value)s)
                     ON CONFLICT (id) DO UPDATE
                     SET
-                        event_ts = EXCLUDED.event_ts
-                        event_type = EXCLUDED.event_type
+                        event_ts = EXCLUDED.event_ts,
+                        event_type = EXCLUDED.event_type,
                         event_value = EXCLUDED.event_value;
                 """,
                 {
