687afbd803ed433c8463100d341d80e4de28b269
«strukovsa» <serrstr07@gmail.com>
2024-04-17 17:19:36 +0300

    dm_rest unfinished

---
+++ dags/examples/stg/dm_restaurants_dag/dm_restaurants_dag.py
@@ -0,0 +1,37 @@
+import logging
+
+import pendulum
+from airflow.decorators import dag, task
+from examples.stg.dm_users_dag.dm_users_loader import DmUsersLoader
+from lib import ConnectionBuilder
+
+log = logging.getLogger(__name__)
+
+@dag(
+    schedule_interval='0/15 * * * *',  # Задаем расписание выполнения дага - каждый 15 минут.
+    start_date=pendulum.datetime(2022, 5, 5, tz="UTC"),  # Дата начала выполнения дага. Можно поставить сегодня.
+    catchup=False,  # Нужно ли запускать даг за предыдущие периоды (с start_date до сегодня) - False (не нужно).
+    tags=['sprint5', 'stg', 'origin', 'example'],  # Теги, используются для фильтрации в интерфейсе Airflow.
+    is_paused_upon_creation=True  # Остановлен/запущен при появлении. Сразу запущен.
+)
+def sprint5_example_dds_users_dag():
+    # Создаем подключение к базе dwh.
+    dwh_pg_connect = ConnectionBuilder.pg_conn("PG_WAREHOUSE_CONNECTION")
+
+    # Объявляем таск, который загружает данные.
+    @task(task_id="users_load")
+    def load_users():
+        # создаем экземпляр класса, в котором реализована логика.
+        rest_loader = DmUsersLoader(dwh_pg_connect, dwh_pg_connect, log)
+        rest_loader.load_users()  # Вызываем функцию, которая перельет данные.
+
+    # Инициализируем объявленные таски.
+    ranks_dict = load_users()
+
+    # Далее задаем последовательность выполнения тасков.
+    # Т.к. таск один, просто обозначим его здесь.
+    ranks_dict  # type: ignore
+
+
+stg_dds_users_dag = sprint5_example_dds_users_dag()
+
---
+++ dags/examples/stg/dm_restaurants_dag/dm_restaurants_loader.py
@@ -0,0 +1,118 @@
+from logging import Logger
+from typing import List
+
+from examples.stg import EtlSetting, StgEtlSettingsRepository
+from lib import PgConnect
+from lib.dict_util import json2str
+from psycopg import Connection
+from psycopg.rows import class_row
+from pydantic import BaseModel
+import json
+
+class DmRestaurantsObj(BaseModel):
+    id: int
+    object_id: str
+    object_value: str
+   
+class DmRestaurantsDdsObj(BaseModel):
+    user_id: str
+    user_name: str
+    user_login: str
+
+class DmRestaurantsOriginRepository:
+    def __init__(self, pg: PgConnect) -> None:
+        self._db = pg
+
+    def list_users(self, user_threshold: int, limit: int) -> List[DmRestaurantsObj]:
+        with self._db.client().cursor(row_factory=class_row(DmUsersObj)) as cur:
+            cur.execute(
+                """
+                    SELECT 
+                        id,
+                        object_id,
+                        object_value
+                    FROM stg.ordersystem_users
+                    WHERE id > %(threshold)s --Пропускаем те объекты, которые уже загрузили.
+                    ORDER BY id ASC --Обязательна сортировка по id, т.к. id используем в качестве курсора.
+                    LIMIT %(limit)s; --Обрабатываем только одну пачку объектов.
+                """, {
+                    "threshold": user_threshold,
+                    "limit": limit
+                }
+            )
+            objs = cur.fetchall()
+        return objs
+
+
+class DmUsersDestRepository:
+
+    def insert_user(self, conn: Connection, user: DmUsersDdsObj) -> None:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                    INSERT INTO dds.dm_users(user_id, user_name, user_login)
+                    VALUES (%(user_id)s, %(user_name)s, %(user_login)s);
+                """,
+                {
+                     "user_id": user.user_id,
+                     "user_name": user.user_name,
+                     "user_login": user.user_login
+                },
+            )
+
+
+class DmUsersLoader:
+    WF_KEY = "example_users_stg_to_dds_workflow"
+    LAST_LOADED_ID_KEY = "last_loaded_id"
+    BATCH_LIMIT = 10000  # Рангов мало, но мы хотим продемонстрировать инкрементальную загрузку рангов.
+
+    def __init__(self, pg_origin: PgConnect, pg_dest: PgConnect, log: Logger) -> None:
+        self.pg_dest = pg_dest
+        self.origin = DmUsersOriginRepository(pg_origin)
+        self.stg = DmUsersDestRepository()
+        self.settings_repository = StgEtlSettingsRepository()
+        self.log = log
+
+    def load_users(self):
+        # открываем транзакцию.
+        # Транзакция будет закоммичена, если код в блоке with пройдет успешно (т.е. без ошибок).
+        # Если возникнет ошибка, произойдет откат изменений (rollback транзакции).
+        with self.pg_dest.connection() as conn:
+
+            # Прочитываем состояние загрузки
+            # Если настройки еще нет, заводим ее.
+            wf_setting = self.settings_repository.get_setting(conn, self.WF_KEY)
+            if not wf_setting:
+                wf_setting = EtlSetting(id=0, workflow_key=self.WF_KEY, workflow_settings={self.LAST_LOADED_ID_KEY: -1})
+
+            # Map DmUsersObj to DmUsersDdsObj
+            def map_user(user: DmUsersObj) -> DmUsersDdsObj:
+                object_value_dict = json.loads(user.object_value)
+                return DmUsersDdsObj(
+                user_id=str(object_value_dict["_id"]),
+                user_name=object_value_dict["name"],
+                user_login=object_value_dict["login"]
+            )
+
+            # Вычитываем очередную пачку объектов.
+            last_loaded = wf_setting.workflow_settings[self.LAST_LOADED_ID_KEY]
+            load_queue = self.origin.list_users(last_loaded, self.BATCH_LIMIT)
+            self.log.info(f"Found {len(load_queue)} users to load.")
+            if not load_queue:
+                self.log.info("Quitting.")
+                return
+
+            # Сохраняем объекты в базу dwh.
+            for user in load_queue:
+                mapped_user = map_user(user)
+                self.stg.insert_user(conn, mapped_user)
+
+            # Сохраняем прогресс.
+            # Мы пользуемся тем же connection, поэтому настройка сохранится вместе с объектами,
+            # либо откатятся все изменения целиком.
+            wf_setting.workflow_settings[self.LAST_LOADED_ID_KEY] = max([t.id for t in load_queue])
+            wf_setting_json = json2str(wf_setting.workflow_settings)  # Преобразуем к строке, чтобы положить в БД.
+            self.settings_repository.save_setting(conn, wf_setting.workflow_key, wf_setting_json)
+
+            self.log.info(f"Load finished on {wf_setting.workflow_settings[self.LAST_LOADED_ID_KEY]}")
+
