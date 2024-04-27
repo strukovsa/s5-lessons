8eab25c9fc9d5ab1c9bc66050abe8c025b3bbb8d
«strukovsa» <serrstr07@gmail.com>
2024-04-27 09:41:29 +0300

    fct v8

--- dags/examples/stg/fct_product_sales_dag/fct_product_sales_loader.py
+++ dags/examples/stg/fct_product_sales_dag/fct_product_sales_loader.py
@@ -1,5 +1,6 @@
 from logging import Logger
-from typing import List, Optional
+from typing import List
+from datetime import datetime
 
 from examples.stg import EtlSetting, StgEtlSettingsRepository
 from lib import PgConnect
@@ -7,156 +8,90 @@ from lib.dict_util import json2str
 from psycopg import Connection
 from psycopg.rows import class_row
 from pydantic import BaseModel
-import json
-from datetime import datetime
-import logging
 
-logging.basicConfig(level=logging.INFO)
 
-class FctObj(BaseModel):
+class RestObj(BaseModel):
     id: int
-    object_id: str
-    object_value: str
-    update_ts: datetime
-   
-class FctDdsObj(BaseModel):
     product_id: int
     order_id: int
     count: int
-    price: float
+    price:float
     total_sum: float
     bonus_payment: float
     bonus_grant: float
 
-class FctOriginRepository:
+
+class RestOriginRepository:
     def __init__(self, pg: PgConnect) -> None:
         self._db = pg
-        self.logger = logging.getLogger(__name__)
 
-    def list_fct(self, order_threshold: int, limit: int) -> List[FctObj]:
-        with self._db.client().cursor(row_factory=class_row(FctObj)) as cur:
+    def list_rest(self, rest_threshold: int, limit: int) -> List[RestObj]:
+        with self._db.client().cursor(row_factory=class_row(RestObj)) as cur:
             cur.execute(
                 """
-                    SELECT 
-                        id,
-                        object_id,
-                        object_value,
-                        update_ts
-                    FROM stg.ordersystem_orders
-                    WHERE id > %(threshold)s --Пропускаем те объекты, которые уже загрузили.
-                    ORDER BY id ASC --Обязательна сортировка по id, т.к. id используем в качестве курсора.
-                    LIMIT %(limit)s; --Обрабатываем только одну пачку объектов.
+                    select df.id,
+       p.id as product_id,
+       o.id as order_id,
+       df.quantity as count,
+       df.price,
+       df.quantity * df.price as total_sum,
+       df.bonus_payment,
+       df.bonus_grant
+from (
+           select id,
+           cast(object_value::json->>'_id' as varchar) as ord_id,
+           cast(object_value::json->>'bonus_payment' as float) as bonus_payment,
+           cast(object_value::json->>'bonus_grant' as float) as bonus_grant,
+           cast(json_array_elements((object_value::json ->> 'order_items')::json)->>'price' as float) as price,
+           cast(json_array_elements((object_value::json ->> 'order_items')::json)->>'quantity' as int) as quantity,
+           cast(json_array_elements((object_value::json ->> 'order_items')::json)->>'name' as varchar) as prod_name
+           from stg.ordersystem_orders) df
+                left join dds.dm_products p on p.product_name = df.prod_name
+                left join dds.dm_orders o on o.order_key = df.ord_id
+                    WHERE df.id > %(threshold)s --Пропускаем те объекты, которые уже загрузили.
+                    ORDER BY df.id ASC --Обязательна сортировка по id, т.к. id используем в качестве курсора.;
                 """, {
-                    "threshold": order_threshold,
-                    "limit": limit
+                    "threshold": rest_threshold
                 }
             )
             objs = cur.fetchall()
         return objs
-    
-    def get_product_id(self, product_id: str) -> Optional[int]:
-        with self._db.client().cursor() as cur:
-            cur.execute(
-                """
-                    SELECT id
-                    FROM dds.dm_products
-                    WHERE product_id = %(product_id)s
-                    AND active_to = '2099-12-31 00:00:00.000';
-                """,
-                {"product_id": product_id},
-            )
-            result = cur.fetchone()
-            if result:
-                return result[0]
-        raise Exception("No result found for the given product_id")
-    
-    def get_order_id(self, order_id: str) -> Optional[int]:
-        self.logger.info(f"Getting order ID for order_id: {order_id}")
-        with self._db.client().cursor() as cur:
-            cur.execute(
-                """
-                    SELECT id
-                    FROM dds.dm_orders
-                    WHERE order_key = %(order_id)s;
-                """,
-                {"order_id": order_id},
-            )
-            result = cur.fetchone()
-            if result:
-                self.logger.info(f"Found order ID: {result[0]} for order_id: {order_id}")
-                return result[0]
-        self.logger.error(f"No result found for the given order_id: {order_id}")
-        raise Exception("No result found for the given order_id")
-    
-    def get_bonus_payment(self, order_id: str) -> Optional[int]:
-        with self._db.client().cursor() as cur:
-            cur.execute(
-                """
-                    SELECT product_payment->>'bonus_payment' AS bonus_payment
-                    FROM stg.bonussystem_events,
-                        json_array_elements(event_value::json->'product_payments') AS product_payment
-                    WHERE event_value::json->>'order_id' = %(order_id)s;
 
-                """,
-                {"order_id": order_id},
-            )
-            result = cur.fetchone()
-            if result:
-                return result[0]
-        raise Exception("No result found for the given order_id")
-    
-    def get_bonus_grant(self, order_id: str) -> Optional[int]:
-        with self._db.client().cursor() as cur:
-            cur.execute(
-                """
-                    SELECT product_payment->>'bonus_grant' AS bonus_grant
-                    FROM stg.bonussystem_events,
-                        json_array_elements(event_value::json->'product_payments') AS product_payment
-                    WHERE event_value::json->>'order_id' = %(order_id)s;
 
-                """,
-                {"order_id": order_id},
-            )
-            result = cur.fetchone()
-            if result:
-                return result[0]
-        raise Exception("No result found for the given order_id")
+class RestDestRepository:
 
-class FctDestRepository:
-
-    def insert_fct(self, conn: Connection, fcts: List[FctDdsObj]) -> None:
+    def insert_rest(self, conn: Connection, rest: RestObj) -> None:
         with conn.cursor() as cur:
-            for fct in fcts:
-                cur.execute("""
+            cur.execute(
+                """
                     INSERT INTO dds.fct_product_sales(product_id, order_id, count, price, total_sum, bonus_payment, bonus_grant)
                     VALUES (%(product_id)s, %(order_id)s, %(count)s, %(price)s, %(total_sum)s, %(bonus_payment)s, %(bonus_grant)s);
-                    """,
-                    {
-                     "product_id": fct.product_id,
-                     "order_id": fct.order_id,
-                     "count": fct.count,
-                     "price": fct.price, 
-                     "total_sum": fct.total_sum,
-                     "bonus_payment": fct.bonus_payment,
-                     "bonus_grant": fct.bonus_grant,
+                """,
+                {
+                     "product_id": rest.product_id,
+                     "order_id": rest.order_id,
+                     "count": rest.count,
+                     "price": rest.price,
+                     "total_sum": rest.total_sum,
+                     "bonus_payment": rest.bonus_payment,
+                     "bonus_grant": rest.bonus_grant
+                },
+            )
 
-                    },
-                    )
 
-
-class FctLoader:
-    WF_KEY = "example_fct_stg_to_dds_workflow"
+class RestLoader:
+    WF_KEY = "sales"
     LAST_LOADED_ID_KEY = "last_loaded_id"
-    BATCH_LIMIT = 3000  # Рангов мало, но мы хотим продемонстрировать инкрементальную загрузку рангов.
+    BATCH_LIMIT = 1000  # Рангов мало, но мы хотим продемонстрировать инкрементальную загрузку рангов.
 
     def __init__(self, pg_origin: PgConnect, pg_dest: PgConnect, log: Logger) -> None:
         self.pg_dest = pg_dest
-        self.origin = FctOriginRepository(pg_origin)
-        self.stg = FctDestRepository()
+        self.origin = RestOriginRepository(pg_origin)
+        self.stg = RestDestRepository()
         self.settings_repository = StgEtlSettingsRepository()
         self.log = log
 
-    def load_fct(self):
+    def load_rest(self):
         # открываем транзакцию.
         # Транзакция будет закоммичена, если код в блоке with пройдет успешно (т.е. без ошибок).
         # Если возникнет ошибка, произойдет откат изменений (rollback транзакции).
@@ -168,53 +103,17 @@ class FctLoader:
             if not wf_setting:
                 wf_setting = EtlSetting(id=0, workflow_key=self.WF_KEY, workflow_settings={self.LAST_LOADED_ID_KEY: -1})
 
-            # Map FctObj to FctDdsObj
-            def map_fct(fct: FctObj) -> List[FctDdsObj]:
-                object_value_dict = json.loads(fct.object_value)
-                product_ids = [item["id"] for item in object_value_dict["order_items"]]
-                correct_product_ids = [self.origin.get_product_id(product_id) for product_id in product_ids]
-                for correct_product_id in correct_product_ids:
-                    if correct_product_id is None:
-                        raise ValueError(f"Could not find a matching product_id for {product_id}.")
-                order_id = fct.object_id
-                correct_order_id = self.origin.get_order_id(order_id)
-                old_order_id = fct.object_id
-                if correct_order_id is None:
-                    raise ValueError(f"Could not find a matching order_id for {order_id}.")
-                counts=[item["quantity"] for item in object_value_dict["order_items"]]
-                prices=[item["price"] for item in object_value_dict["order_items"]]
-
-                mapped_fcts = []
-                for product_id, count, price in zip(correct_product_ids, counts, prices):
-                    if product_id is not None:
-                        mapped_fcts.append(
-                        FctDdsObj(
-                        order_id=correct_order_id,
-                        product_id=int(correct_product_id),
-                        count=int(count),
-                        price=float(price),
-                        total_sum=int(count) * float(price),
-                        bonus_payment=self.origin.get_bonus_payment(old_order_id),
-                        bonus_grant=self.origin.get_bonus_grant(old_order_id),  
-                        )
-                        )
-                return mapped_fcts
-                
-            
-
             # Вычитываем очередную пачку объектов.
             last_loaded = wf_setting.workflow_settings[self.LAST_LOADED_ID_KEY]
-            load_queue = self.origin.list_fct(last_loaded, self.BATCH_LIMIT)
-            self.log.info(f"Found {len(load_queue)} fcts to load.")
+            load_queue = self.origin.list_rest(last_loaded, self.BATCH_LIMIT)
+            self.log.info(f"Found {len(load_queue)} rest to load.")
             if not load_queue:
                 self.log.info("Quitting.")
                 return
 
             # Сохраняем объекты в базу dwh.
-            for fct in load_queue:
-                mapped_fct = map_fct(fct)
-                if mapped_fct:
-                    self.stg.insert_fct(conn, mapped_fct)
+            for rest in load_queue:
+                self.stg.insert_rest(conn, rest)
 
             # Сохраняем прогресс.
             # Мы пользуемся тем же connection, поэтому настройка сохранится вместе с объектами,
@@ -224,4 +123,5 @@ class FctLoader:
             self.settings_repository.save_setting(conn, wf_setting.workflow_key, wf_setting_json)
 
             self.log.info(f"Load finished on {wf_setting.workflow_settings[self.LAST_LOADED_ID_KEY]}")
+
 
